934448c77ea1ca0386f080071c8c7286df147263
Enchan <enchant0130@gmail.com>
2023-09-02 22:31:28 +0900

    [Update] large file

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import hashlib
 
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
@@ -26,13 +27,20 @@ def main() -> int:
         print(f"Environment variable {folder_id_key} is not set.")
         return 1
 
+    # メチャクチャなサイズのファイルを作る
     file_name = "example.txt"
-    with open(file_name, "w") as f:
-        f.write("Hello, World!")
+    sha256_hash = hashlib.sha256()
+    with open(file_name, "wb") as f:
+        data = os.urandom(10000000)
+        sha256_hash.update(data)
+        f.write(data)
+
+    # ハッシュを出力しておく
+    print(f"file SHA-256 hash: {sha256_hash.hexdigest()}")
 
     # APIを叩く
     with build("drive", "v3", credentials=credentials) as drive_service:
-        media = MediaFileUpload(file_name, mimetype='text/plain')
+        media = MediaFileUpload(file_name, mimetype='text/plain', resumable=True, chunksize=1000000)
         file_metadata = {'name': file_name, 'parents': [folder_id]}
         drive_service.files().create(body=file_metadata, media_body=media).execute()
 
